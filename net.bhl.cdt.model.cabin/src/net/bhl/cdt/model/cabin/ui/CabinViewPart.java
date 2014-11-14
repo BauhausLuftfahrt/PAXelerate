80f8a9858eba4ea970da4c56d859e2ae26b62417
marc.engelmann <marc.engelmann@bauhaus-luftfahrt.net>
2014-11-14 13:24:53 +0100

    Added seats to image

--- net.bhl.cdt.model.cabin/src/net/bhl/cdt/model/cabin/ui/CabinViewPart.java
+++ net.bhl.cdt.model.cabin/src/net/bhl/cdt/model/cabin/ui/CabinViewPart.java
@@ -2,6 +2,7 @@ package net.bhl.cdt.model.cabin.ui;
 
 import net.bhl.cdt.model.cabin.Cabin;
 import net.bhl.cdt.model.cabin.CabinFactory;
+import net.bhl.cdt.model.cabin.Door;
 
 import org.eclipse.jface.viewers.ITableLabelProvider;
 import org.eclipse.jface.viewers.LabelProvider;
@@ -22,6 +23,9 @@ public class CabinViewPart extends ViewPart {
 	private TableViewer viewer;
 	public int x_zero;
 	public int y_zero;
+	public Cabin newCabin;
+	public int cabin_x;
+	public int cabin_y;
 
 	class ViewLabelProvider extends LabelProvider implements ITableLabelProvider {
 		public String getColumnText(Object obj, int index) {
@@ -41,28 +45,53 @@ public class CabinViewPart extends ViewPart {
 	/** * This is a callback that will allow us to create the viewer and initialize * it. */
 
 	public void createPartControl(Composite parent) {
-		final Image image = new Image(parent.getDisplay(),"C:\\Users\\Marc.Engelmann\\Desktop\\lh_a320_cut.PNG"); // warum geht das nur mit absolutem Pfad?
-		x_zero = 140;
+		getElement();
+		//Door door = CabinFactory.eINSTANCE.createDoor();
+		
+		// warum geht das nur mit absolutem Pfad?
+		final Image image = new Image(parent.getDisplay(),"C:\\Users\\Marc.Engelmann\\Desktop\\lh_a320_cut.PNG"); 
+		// falsch!!!!!!!!!!
+		x_zero = 139;
 		y_zero = 90;
+		cabin_x = 94;
+		cabin_y = 636;
+		final int seatWidth = (cabin_x - 16) / 6 - 2;
+		final int seatLength = 10;
+		final int seatPitch = 10;
+		final int numbRows = cabin_y / (seatLength+seatPitch);
 		Canvas canvas = new Canvas(parent, SWT.RESIZE);
 		canvas.addPaintListener(new PaintListener() {
 		      public void paintControl(final PaintEvent e) { 
 		    	  e.gc.drawImage(image, 0, 0);
 		    	  //e.gc.drawText(testStr, 0, 0);
-		    	  //e.gc.drawText("Test Message", 0,0);
+		    	  e.gc.drawText("Layout not based\non actual cabin model!", 0,0);
 		    	  e.gc.setBackground(e.display.getSystemColor(SWT.COLOR_GRAY));
 		    	  //e.gc.setAlpha(200);
-		    	  e.gc.fillRectangle(x_zero, y_zero, 93, 636);
-		    	  //e.gc.drawT     
+		    	  e.gc.fillRectangle(x_zero, y_zero, cabin_x, cabin_y);
+		    	  e.gc.setBackground(e.display.getSystemColor(SWT.COLOR_DARK_GRAY));
+		    	  for (int j = 0; j<numbRows; j++) {
+		    		  for (int i = 0; i <= 6; i++) {
+		    			  if (i!=3) {
+		    				  // besser direkt die Koordinaten der Sitze auslesen!
+		    				  e.gc.drawRectangle((x_zero+(seatWidth+2)*i),y_zero +(seatPitch+seatLength)*j + 20,seatWidth,seatLength); 
+		    			  }
+		    		  }
+		    	  }
 		      }
 	    });    
 
 	}
 
-	private Object getElement() {
-		Cabin newCabin = CabinFactory.eINSTANCE.createCabin();
+//	private Object getElement() {
+//		newCabin = CabinFactory.eINSTANCE.createCabin();
+//		newCabin.setName("MyCabin");
+//		return newCabin;
+//	}
+	
+	private void getElement() {
+		newCabin = CabinFactory.eINSTANCE.createCabin();
 		newCabin.setName("MyCabin");
-		return newCabin;
+		//return newCabin;
 	}
 
 	/** * Passing the focus request to the viewer's control. */
