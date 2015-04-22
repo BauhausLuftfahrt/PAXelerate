d92d422490f9ed9212fdef055406de8880daed9f
marc.engelmann <marc.engelmann@bauhaus-luftfahrt.net>
2015-04-22 15:22:39 +0200

    solved several issues concerning the reparing of seat properties after modifications.

--- net.bhl.cdt.model.cabin/src/net/bhl/cdt/model/cabin/commands/DrawCabinCommand.java
+++ net.bhl.cdt.model.cabin/src/net/bhl/cdt/model/cabin/commands/DrawCabinCommand.java
@@ -6,11 +6,17 @@
 package net.bhl.cdt.model.cabin.commands;
 
 import net.bhl.cdt.commands.CDTCommand;
+import net.bhl.cdt.model.cabin.BusinessClass;
 import net.bhl.cdt.model.cabin.Cabin;
+import net.bhl.cdt.model.cabin.EconomyClass;
+import net.bhl.cdt.model.cabin.FirstClass;
 import net.bhl.cdt.model.cabin.Passenger;
 import net.bhl.cdt.model.cabin.PhysicalObject;
+import net.bhl.cdt.model.cabin.PremiumEconomyClass;
+import net.bhl.cdt.model.cabin.Row;
 import net.bhl.cdt.model.cabin.Seat;
 import net.bhl.cdt.model.cabin.ui.CabinViewPart;
+import net.bhl.cdt.model.cabin.util.FuncLib;
 import net.bhl.cdt.model.util.ModelHelper;
 
 import org.eclipse.core.runtime.ILog;
@@ -68,7 +74,7 @@ public class DrawCabinCommand extends CDTCommand {
 
 		checkPassengerAssignments();
 
-		reassignSeatIds();
+		repairSeatAssignments();
 
 		try {
 			cabinViewPart.setCabin(cabin);
@@ -91,10 +97,58 @@ public class DrawCabinCommand extends CDTCommand {
 		}
 	}
 
-	private void reassignSeatIds() {
+	private void repairSeatAssignments() {
 		int i = 1;
 		for (Seat seat : ModelHelper.getChildrenByClass(cabin, Seat.class)) {
+
 			seat.setId(i);
+			int seatnumber = i % 6;
+			if (seatnumber == 0) {
+				seatnumber = 6;
+			}
+
+			int rownumber = ((int) ((i - 1) / 6) + 1);
+
+			seat.setName(rownumber + FuncLib.getCharForNumber(seatnumber));
+
+			Row row = ModelHelper.getChildrenByClass(cabin, Row.class).get(
+					rownumber - 1);
+			seat.setRow(row);
+
+			int xDim = 0;
+			int yDim = 0;
+			switch (seat.getTravelClass().getClass().getSimpleName()) {
+			case "FirstClass":
+				xDim = ModelHelper.getChildrenByClass(cabin, FirstClass.class)
+						.get(0).getSeatWidth();
+				yDim = ModelHelper.getChildrenByClass(cabin, FirstClass.class)
+						.get(0).getSeatLength();
+			case "BusinessClass":
+				xDim = ModelHelper
+						.getChildrenByClass(cabin, BusinessClass.class).get(0)
+						.getSeatWidth();
+				yDim = ModelHelper
+						.getChildrenByClass(cabin, BusinessClass.class).get(0)
+						.getSeatLength();
+			case "PremiumEconomyClass":
+				xDim = ModelHelper
+						.getChildrenByClass(cabin, PremiumEconomyClass.class)
+						.get(0).getSeatWidth();
+				yDim = ModelHelper
+						.getChildrenByClass(cabin, PremiumEconomyClass.class)
+						.get(0).getSeatLength();
+			default:
+				xDim = ModelHelper
+						.getChildrenByClass(cabin, EconomyClass.class).get(0)
+						.getSeatWidth();
+				yDim = ModelHelper
+						.getChildrenByClass(cabin, EconomyClass.class).get(0)
+						.getSeatLength();
+
+			}
+			seat.setXDimension(xDim);
+			seat.setYDimension(yDim);
+
 			i++;
 		}
 
