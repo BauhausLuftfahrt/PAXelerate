e2003254f4845b7a59bc63f29d5157d36136a172
michael.schmidt <michael.schmidt@bauhaus-luftfahrt.net>
2015-09-14 11:59:46 +0200

    Added EPL information

--- net.bhl.cdt.model.cabin/src/net/bhl/cdt/model/cabin/commands/GenerateCabinCommand.java
+++ net.bhl.cdt.model.cabin/src/net/bhl/cdt/model/cabin/commands/GenerateCabinCommand.java
@@ -1,9 +1,8 @@
 /*******************************************************************************
- * <copyright> Copyright (c) 2009-2014 Bauhaus Luftfahrt e.V.. All rights reserved. This program and the accompanying
+ * <copyright> Copyright (c) 2014-2015 Bauhaus Luftfahrt e.V.. All rights reserved. This program and the accompanying
  * materials are made available under the terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html </copyright>
- *******************************************************************************/
-
+ ***************************************************************************************/
 package net.bhl.cdt.model.cabin.commands;
 
 import net.bhl.cdt.commands.CDTCommand;
