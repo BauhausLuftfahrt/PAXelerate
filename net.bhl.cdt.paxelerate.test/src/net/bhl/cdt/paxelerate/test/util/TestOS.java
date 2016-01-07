2daad7d27dea71785ffe3001e16b8209c81caf8b
RRothfeld <raoul.rothfeld@bauhaus-luftfahrt.net>
2016-01-07 11:01:11 +0100

    Commented TestOS and refactored OS.java

--- net.bhl.cdt.paxelerate.test/src/net/bhl/cdt/paxelerate/test/util/TestOS.java
+++ net.bhl.cdt.paxelerate.test/src/net/bhl/cdt/paxelerate/test/util/TestOS.java
@@ -1,3 +1,9 @@
+/*******************************************************************************
+ * <copyright> Copyright (c) 2009-2016 Bauhaus Luftfahrt e.V.. All rights reserved. This program and the accompanying
+ * materials are made available under the terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html </copyright>
+ ******************************************************************************/
+
 package net.bhl.cdt.paxelerate.test.util;
 
 import static org.junit.Assert.*;
@@ -6,7 +12,13 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import net.bhl.cdt.paxelerate.util.OS;
+import net.bhl.cdt.paxelerate.util.OS.OSType;
 
+/**
+ * Test class for OS.java imported from net.bhl.cdt.paxelerate.util
+ * 
+ * @author raoul.rothfeld
+ */
 public class TestOS {
 
 	@BeforeClass
@@ -21,7 +33,6 @@ public class TestOS {
 
 		// Record number of true responses for each OS possibility
 		int actualResponses = 0;
-
 		if (OS.isWindows())
 			actualResponses++;
 		if (OS.isMac())
@@ -32,7 +43,19 @@ public class TestOS {
 			actualResponses++;
 
 		// Test if exactly one OS has been recognized
-		assertEquals(expectedResponses, actualResponses);
+		assertEquals("No or more than one OS detected.", expectedResponses, actualResponses);
 	}
 
+	@Test
+	public void OSTypeTest() {
+		// Retrieve active OS
+		OSType activeOS = OS.getOSType();
+
+		// Check if OS is listed as enumerator in OSType
+		boolean found = false;
+		for (OSType type : OSType.values())
+			if (!found)
+				found = (type == activeOS);
+		assertTrue("OS type unrecognised.", found);
+	}
 }
