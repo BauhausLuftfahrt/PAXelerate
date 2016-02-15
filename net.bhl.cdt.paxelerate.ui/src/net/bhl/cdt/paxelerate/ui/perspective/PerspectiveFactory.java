2c5766e9565c5fe0ab16b8be9b8f5e8a264174ea
RRothfeld <raoul.rothfeld@bauhaus-luftfahrt.net>
2016-02-15 14:01:22 +0100

    show own perspective on startup

--- net.bhl.cdt.paxelerate.ui/src/net/bhl/cdt/paxelerate/ui/perspective/PerspectiveFactory.java
+++ net.bhl.cdt.paxelerate.ui/src/net/bhl/cdt/paxelerate/ui/perspective/PerspectiveFactory.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * <copyright> Copyright (c) 2009-2014 Bauhaus Luftfahrt e.V.. All rights reserved. This program and the accompanying
+ * <copyright> Copyright (c) 2009-2016 Bauhaus Luftfahrt e.V.. All rights reserved. This program and the accompanying
  * materials are made available under the terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html </copyright>
  ******************************************************************************/
@@ -18,29 +18,21 @@ import org.eclipse.ui.console.IConsoleConstants;
  * @author raoul.rothfeld
  *
  */
-@SuppressWarnings("restriction")
 public class PerspectiveFactory implements IPerspectiveFactory {
-
-	private IPageLayout layout;
 
 	@Override
 	public void createInitialLayout(IPageLayout layout) {
-		this.layout = layout;
-		addViews();
-	}
-
-	private void addViews() {
 		
 		IFolderLayout left = layout.createFolder("left", IPageLayout.LEFT, 0.25f, layout.getEditorArea());
 		left.addView(ModelExplorerView.ID);
-		
-		System.err.println("AH§R=()=(HE=(HER=H(FEH=(EF=H(EF=(H");
 
 		IFolderLayout right = layout.createFolder("right", IPageLayout.RIGHT, 0.70f, layout.getEditorArea());
-		//right.addView("net.bhl.cdt.paxelerate.ui.views.cabinview");
+		right.addView("net.bhl.cdt.paxelerate.ui.views.cabinview");
+		right.addView(ModelRepositoriesView.ID);
+		
 
 		IFolderLayout bottom = layout.createFolder("bottom", IPageLayout.BOTTOM, 0.70f, layout.getEditorArea());
-		//bottom.addView(IConsoleConstants.ID_CONSOLE_VIEW);
+		bottom.addView(IConsoleConstants.ID_CONSOLE_VIEW);
 	}
 
 }
--- net.bhl.cdt.paxelerate.ui/src/net/bhl/cdt/paxelerate/ui/views/CabinViewPart.java
+++ net.bhl.cdt.paxelerate.ui/src/net/bhl/cdt/paxelerate/ui/views/CabinViewPart.java
@@ -3,7 +3,6 @@
  * materials are made available under the terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html </copyright>
  ******************************************************************************/
-
 package net.bhl.cdt.paxelerate.ui.views;
 
 import java.io.File;
