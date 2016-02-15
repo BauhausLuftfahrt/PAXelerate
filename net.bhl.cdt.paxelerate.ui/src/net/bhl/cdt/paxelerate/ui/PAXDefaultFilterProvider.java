bfc7ab55c754a7532a917c64f1405be5f952f794
michael.schmidt <michael.schmidt@bauhaus-luftfahrt.net>
2016-02-15 10:22:06 +0100

    added FilterProvider to preselect PAXelerate ECore model

---
+++ net.bhl.cdt.paxelerate.ui/src/net/bhl/cdt/paxelerate/ui/PAXDefaultFilterProvider.java
@@ -0,0 +1,40 @@
+/*******************************************************************************
+ * <copyright> Copyright (c) 2009-2016 Bauhaus Luftfahrt e.V.. All rights reserved. This program and the accompanying
+ *  materials are made available under the terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ *  and is available at http://www.eclipse.org/legal/epl-v10.html </copyright>
+ ******************************************************************************/
+package net.bhl.cdt.paxelerate.ui;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+import org.eclipse.emf.ecore.EPackage.Registry;
+import org.eclipse.emf.ecp.core.util.ECPFilterProvider;
+
+/**
+ * Filter all EPackages not associated with .cdt.paxelerate.
+ * Used by ExtensionPoint filter.
+ * 
+ * @author David Otter, Michael Schmidt
+ *
+ */
+public class PAXDefaultFilterProvider implements ECPFilterProvider {
+
+	@Override
+	public Set<String> getHiddenPackages() {
+		String temp = null;
+		Set<String> hiddenElements = new HashSet<String>();
+		//All URIs
+		Set<String> relevantURIs = new HashSet<String>(Registry.INSTANCE.keySet());
+		Iterator<String> iterator = relevantURIs.iterator();
+		while (iterator.hasNext()){
+			temp = iterator.next();
+			if (!temp.contains("cabin")) {
+				hiddenElements.add(temp);
+			}
+		}
+		return hiddenElements;
+	}
+
+}
