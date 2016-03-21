ee8b9bdedf2132145b8f7a3d5c8a242d9951dde9
michael.schmidt <michael.schmidt@bauhaus-luftfahrt.net>
2016-03-21 11:11:50 +0100

    closes #32 basic information added to WelcomePage

--- net.bhl.cdt.paxelerate.ui/src/net/bhl/cdt/paxelerate/ui/WelcomePage.java
+++ net.bhl.cdt.paxelerate.ui/src/net/bhl/cdt/paxelerate/ui/WelcomePage.java
@@ -17,10 +17,10 @@ import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.ExpandBar;
 import org.eclipse.swt.widgets.ExpandItem;
+import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.Scale;
-import org.eclipse.swt.widgets.Slider;
-import org.eclipse.swt.widgets.Spinner;
+import org.eclipse.swt.widgets.Link;
+import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.intro.IIntroPart;
 import org.eclipse.ui.part.IntroPart;
@@ -36,39 +36,35 @@ public class WelcomePage extends IntroPart implements IIntroPart {
 	@Override
 	public void createPartControl(Composite parent) {
 		Composite outerContainer = new Composite(parent, SWT.NONE);
-		RowLayout rowLayout = new RowLayout();
-		rowLayout.wrap = true;
-		rowLayout.center = true;
-		rowLayout.type = SWT.VERTICAL;
-		rowLayout.marginBottom = rowLayout.marginLeft = rowLayout.marginRight = rowLayout.marginTop = 10;
-		outerContainer.setLayout(rowLayout);
-		
+		GridLayout gridLayout = new GridLayout();
+		gridLayout.marginLeft = gridLayout.marginTop = gridLayout.marginRight = gridLayout.marginBottom = 10;
+		gridLayout.verticalSpacing = 10;
+		outerContainer.setLayout(gridLayout);
+			
 		// Welcome message
 		Label newWelcomeLabel = new Label(outerContainer, SWT.NONE);
-		newWelcomeLabel.setLayoutData(new RowData(800,50));
-		newWelcomeLabel.setText("Welcome to PAXelerate - an open source passenger flow simulation framework for advanced aircraft cabin layouts!");
+		GridData gridData = new GridData(SWT.CENTER, SWT.TOP, true, false);
+		newWelcomeLabel.setLayoutData(gridData);
+		newWelcomeLabel.setText("Welcome to PAXelerate - an open source passenger flow simulation framework "
+				+ "for advanced aircraft cabin layouts!");
 		
-	    // ExpandBar
-		ExpandBar bar = new ExpandBar (outerContainer, SWT.V_SCROLL);
+		// Group #1
+		Group group1 = new Group(outerContainer, SWT.NONE);
+	    group1.setText("New Project");
+	    gridData = new GridData(SWT.CENTER, SWT.TOP, true, false);
+	    group1.setLayoutData(gridData);
+	    
+		Label label1 = new Label (group1, SWT.NONE);
+		label1.setText("To get started, create a new project and select EMF Store as a provider. ");
+		label1.setLocation(10,20);
+		label1.pack();
 		
-		// First item
-		//Composite composite = new Composite (bar, SWT.NONE);
-		//composite.setLayoutData(new RowData(800,50));
-		
-		Composite composite = new Composite (bar, SWT.NONE);
-		GridLayout layout = new GridLayout ();
-		layout.marginLeft = layout.marginTop = layout.marginRight = layout.marginBottom = 10;
-		layout.verticalSpacing = 10;
-		composite.setLayout(layout);
-		
-		Label label0 = new Label (composite, SWT.NONE);
-		label0 = new Label (composite, SWT.NONE);
-		label0.setText("To get started, create a new project and select EMF Store as a provider. ");
-		
-		Button newProjectButton = new Button(composite, SWT.PUSH);
+		Button newProjectButton = new Button(group1, SWT.PUSH);
 		newProjectButton.setText("New Project");
-		newProjectButton.setLayoutData(new GridData());
+		newProjectButton.setLayoutData(gridData);
 		newProjectButton.computeSize(SWT.DEFAULT, SWT.DEFAULT);
+		newProjectButton.setLocation(150,40);
+		
 		newProjectButton.addSelectionListener(new SelectionListener() {
 
 			@Override
@@ -84,35 +80,37 @@ public class WelcomePage extends IntroPart implements IIntroPart {
 				// do nothing
 			}
 		});
+		newProjectButton.pack();
+		group1.pack();
 		
-		ExpandItem item0 = new ExpandItem (bar, SWT.NONE, 0);
-		item0.setText("New Project");
-		item0.setHeight(composite.computeSize(SWT.DEFAULT, SWT.DEFAULT).y);
-		item0.setControl(composite);
-		item0.setExpanded(true);
-		//item0.setImage(image);
-			
-		// Second item
 		
-		composite = new Composite (bar, SWT.NONE);
-		layout = new GridLayout ();
-		layout.marginLeft = layout.marginTop = layout.marginRight = layout.marginBottom = 10;
-		layout.verticalSpacing = 10;
-		composite.setLayout(layout);
-		
-		//composite = new Composite (bar, SWT.NONE);
-		//composite.setLayoutData(new RowData(800,50));
-		
-		ExpandItem item1 = new ExpandItem (bar, SWT.NONE, 1);
-		item1.setText("Tutorial");
-		item1.setHeight(composite.computeSize(SWT.DEFAULT, SWT.DEFAULT).y);
-		item1.setControl(composite);
-		// item1.setImage(image);
-		
-	
-		item1.setExpanded(true);
-		bar.setSpacing(8);
-	
+		// Group #2
+		Group group2 = new Group(outerContainer, SWT.NONE);
+	    group2.setText("First encounter with PAXelerate");
+	    gridData = new GridData(SWT.CENTER, SWT.TOP, true, false);
+	    group2.setLayoutData(gridData);
+	    
+	    Label label2 = new Label (group2, SWT.NONE);
+		label2.setText("1. Add a new Cabin model element with a right click on the aircraft project folder.");
+		label2.setLocation(20,20);
+		label2.pack();
+		label2 = new Label (group2, SWT.NONE);
+		label2.setText("2 Use the Generate New Cabin function to create a first layout.");
+		label2.setLocation(20,40);
+		label2.pack();
+		label2 = new Label (group2, SWT.NONE);
+		label2.setText("3. Add passenger with Generate Passengers");
+		label2.setLocation(20,60);
+		label2.pack();
+		label2 = new Label (group2, SWT.NONE);
+		label2.setText("4. And finally start the boarding simulation");
+		label2.setLocation(20,80);
+		label2.pack();
+		label2 = new Label (group2, SWT.NONE);
+		label2.setText("A more detailed tutorial is provided on the PAXelerate website");
+		label2.setLocation(10,100);
+		label2.pack();
+		group2.pack();			
 	}
 
 	@Override
