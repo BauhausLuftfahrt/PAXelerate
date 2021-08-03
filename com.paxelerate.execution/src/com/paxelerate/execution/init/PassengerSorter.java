3d40b905fa4d109d28cfdf78ec689dcb0b7525f1
Marc Engelmann <16258698+marcengelmann@users.noreply.github.com>
2021-08-03 10:45:56 +0200

    Addition of deactivated code for future features

--- com.paxelerate.execution/src/com/paxelerate/execution/init/PassengerSorter.java
+++ com.paxelerate.execution/src/com/paxelerate/execution/init/PassengerSorter.java
@@ -383,4 +383,445 @@ public interface PassengerSorter {
 			passengers.addAll(column);
 		}
 	}
+
+//	/**
+//	*
+//	*/
+//	private static void sortMilneKelly(EList<Passenger> passengers) {
+//		milneAndKelly(passengers);
+//		int[][] cabinMap = new int[seatAbrest][numberOfRows];
+//		LuggageDistribution.luggageDistributionToConsole(cabinMap);
+//		sortSteffen(passengers);
+//	}
+//
+//	/**
+//	 *
+//	 * @param paxList
+//	 */
+//	private static void milneAndKelly(EList<Passenger> paxList) {
+//
+//		Cabin cabin = CabinExtensions.getBy(paxList.get(0));
+//
+//		int numberOfRows =
+//
+//		// check maximum seat abreast
+//		int seatAbrest = 0;
+//		for (int i = 0; i < numberOfRows; i++) {
+//			if (CabinFunctions.checkSeatAbrest(paxList.get(i).getSeat()) > seatAbrest) {
+//				seatAbrest = CabinFunctions.checkSeatAbrest(paxList.get(i).getSeat());
+//			}
+//		}
+//
+//		// Array with number of Hand Luggage in first column and assignment
+//		int numberOfColumns = seatAbrest;
+//		int[] handLuggage = new int[paxList.size()];
+//
+//		// number of bags per row
+//		int[] numberOfBagsInRow = new int[numberOfRows];
+//
+//		/*
+//		 * TODO STEP 1 - construct array with max bags and list of PAX - number of PAX
+//		 * with a lot of bags divided by rows - rest - spread evenly - check number of
+//		 * zero spots in rows, if they are available, fill them with second largest -
+//		 * continue until end
+//		 */
+//
+//		// determines max bags
+//		int maxLuggage = 0;
+//		for (int i = 0; i < paxList.size(); i++) {
+//			handLuggage[i] = paxList.get(i).getLuggage().size();
+//			if (paxList.get(i).getLuggage().size() > maxLuggage) {
+//				maxLuggage = paxList.get(i).getLuggage().size();
+//			}
+//		}
+//
+//		// initializes cabin
+//		int[][] cabin = new int[numberOfColumns][numberOfRows];
+//		// if seat is not jet assigned with a number of bags its value is 0
+//		for (int row = 0; row < numberOfRows; row++) {
+//			for (int column = 0; column < numberOfColumns; column++) {
+//				// mark cabin positions with no seats in 9 abreast cabin
+//				if (seatAbrest == 9 && nineAbreast300pax && row == numberOfRows - 1 && (column < 3 ^ column > 5)) {
+//					cabin[column][row] = 100;
+//				} else
+//					cabin[column][row] = 0;
+//			}
+//		}
+//
+//		int column = 0;
+//		int numberOfUnassignedRows = numberOfRows;
+//		if (nineAbreast300pax) {
+//			numberOfUnassignedRows = numberOfRows - 1;
+//		}
+//		int[] unasignedRows = new int[numberOfUnassignedRows];
+//		for (int row = 0; row < numberOfRows; row++) {
+//			if (cabin[column][row] == 0) {
+//				unasignedRows[row] = row;
+//			}
+//		}
+//		// start with highest number of bags
+//		for (int bagPieces = maxLuggage; bagPieces > 0; bagPieces--) {
+//			// determines number of PAX with that number of bags
+//			int numberOfUnassignedPaxCarryKBags = 0;
+//			for (int pax = 0; pax < paxList.size(); pax++) {
+//				if (handLuggage[pax] == bagPieces)
+//					numberOfUnassignedPaxCarryKBags++;
+//			}
+//
+//			while (numberOfUnassignedPaxCarryKBags > 0) {
+//				// check column empty
+//				if (unasignedRows.length == numberOfRows) {
+//					if (numberOfUnassignedPaxCarryKBags >= unasignedRows.length) {
+//						// update unassigned rows
+//						unasignedRows = new int[numberOfUnassignedRows];
+//						int i = 0;
+//						for (int row = 0; row < numberOfRows; row++) {
+//							if (cabin[column][row] == 0) {
+//								unasignedRows[i] = row;
+//								i++;
+//							}
+//						}
+//						// fill entire column
+//						for (int row = unasignedRows.length - 1; row >= 0; row--) {
+//							cabin[column][row] = bagPieces;
+//							numberOfUnassignedPaxCarryKBags--;
+//							numberOfBagsInRow[row] = numberOfBagsInRow[row] + bagPieces;
+//						}
+//						column++;
+//						numberOfUnassignedRows = numberOfRows;
+//					} else {
+//						// update unassigned rows
+//						unasignedRows = new int[numberOfUnassignedRows];
+//						int i = 0;
+//						for (int row = 0; row < numberOfRows; row++) {
+//							if (cabin[column][row] == 0) {
+//								unasignedRows[i] = row;
+//								i++;
+//							}
+//						}
+//						// spread bags evenly
+//						double distance = (double) unasignedRows.length
+//								/ (double) (numberOfUnassignedPaxCarryKBags + 1);
+//						boolean alternating = false;
+//						int toAsign = (int) distance;
+//						while (numberOfUnassignedPaxCarryKBags > 0) {
+//							if (alternating) {
+//								cabin[column][toAsign] = bagPieces;
+//								numberOfBagsInRow[toAsign] = numberOfBagsInRow[toAsign] + bagPieces;
+//								numberOfUnassignedRows--;
+//								numberOfUnassignedPaxCarryKBags--;
+//							} else {
+//								cabin[column][toAsign] = bagPieces;
+//								numberOfBagsInRow[toAsign] = numberOfBagsInRow[toAsign] + bagPieces;
+//								numberOfUnassignedRows--;
+//								numberOfUnassignedPaxCarryKBags--;
+//							}
+//							if (alternating) {
+//								if (toAsign + distance + 1 < numberOfRows - 1) {
+//									toAsign = toAsign + (int) distance + 1;
+//									alternating = false;
+//								} else {
+//									toAsign++;
+//								}
+//							} else {
+//								if (toAsign + distance < numberOfRows - 1) {
+//									toAsign = toAsign + (int) distance;
+//									if (distance >= 0.5 + (int) distance && distance != (int) distance) {
+//										alternating = true;
+//									}
+//								} else {
+//									toAsign++;
+//								}
+//							}
+//						}
+//						// update unassigned rows
+//						unasignedRows = new int[numberOfUnassignedRows];
+//						int j = 0;
+//						for (int row = 0; row < numberOfRows; row++) {
+//							if (cabin[column][row] == 0) {
+//								unasignedRows[j] = row;
+//								j++;
+//							}
+//						}
+//					}
+//				}
+//				// column not empty
+//				else {
+//					if (numberOfUnassignedPaxCarryKBags >= unasignedRows.length) {
+//						// fill up column
+//						for (int i = 0; i < unasignedRows.length; i++) {
+//							int row = unasignedRows[i];
+//							cabin[column][row] = bagPieces;
+//							numberOfUnassignedPaxCarryKBags--;
+//							numberOfBagsInRow[row] = numberOfBagsInRow[row] + bagPieces;
+//						}
+//						column++;
+//						if (nineAbreast300pax && (column < 3 ^ column > 5)) {
+//							numberOfUnassignedRows = numberOfRows - 1;
+//						} else {
+//							numberOfUnassignedRows = numberOfRows;
+//						}
+//
+//					} else {
+//						// spread bags evenly
+//						double distance = (double) unasignedRows.length
+//								/ (double) (numberOfUnassignedPaxCarryKBags + 1);
+//						int toAsign = (int) distance;
+//						boolean alternating = true;
+//						while (numberOfUnassignedPaxCarryKBags > 0) {
+//							if (cabin[column][toAsign] == 0) {
+//								// place luggage here
+//								cabin[column][toAsign] = bagPieces;
+//								numberOfUnassignedPaxCarryKBags--;
+//								numberOfBagsInRow[toAsign] = numberOfBagsInRow[toAsign] + bagPieces;
+//								numberOfUnassignedRows--;
+//								if (alternating) {
+//									if (toAsign + distance + 1 < numberOfRows - 1) {
+//										toAsign = toAsign + (int) distance + 1;
+//										alternating = false;
+//									} else {
+//										toAsign++;
+//									}
+//								} else {
+//									if (toAsign + distance < numberOfRows - 1) {
+//										toAsign = toAsign + (int) distance;
+//										if (distance != (int) distance && distance >= 0.5 + (int) distance) {
+//											alternating = true;
+//										}
+//									} else {
+//										toAsign++;
+//									}
+//								}
+//							} else {
+//								// go one +1 row to the back
+//								toAsign++;
+//							}
+//						}
+//					}
+//					// update unassigned rows
+//					unasignedRows = new int[numberOfUnassignedRows];
+//					int i = 0;
+//					for (int row = 0; row < numberOfRows; row++) {
+//						if (cabin[column][row] == 0) {
+//							unasignedRows[i] = row;
+//							i++;
+//						}
+//					}
+//				}
+//			}
+//		}
+//
+//		/*
+//		 * TODO STEP 2 - sort PAX in row decreasing by HL - row 1: assign first left,
+//		 * second right,... - row 2: assign first right, second left,...
+//		 */
+//
+//		// sort array in row decreasing by HL
+//		for (int row = 0; row < numberOfRows; row++) {
+//			if (nineAbreast300pax && row == numberOfRows - 1) {
+//				// leave last row unsorted
+//			} else {
+//				for (int col = 0; col < numberOfColumns - 1; col++) {
+//					for (int otherCol = col; otherCol < numberOfColumns; otherCol++) {
+//						if (cabin[col][row] != 100) {
+//							if (cabin[col][row] < cabin[otherCol][row]) {
+//								int store = cabin[otherCol][row];
+//								cabin[otherCol][row] = cabin[col][row];
+//								cabin[col][row] = store;
+//							}
+//						}
+//					}
+//				}
+//			}
+//		}
+//
+//		// loop through all rows
+//		for (int row = 0; row < numberOfRows; row++) {
+//			boolean alternating = true;
+//			// alternate left right
+//			if (row % 2 == 0) {
+//				alternating = true;
+//			} else {
+//				alternating = false;
+//			}
+//			// loop through all columns
+//			if (nineAbreast300pax && row == numberOfRows - 1) {
+//				// leave last row unsorted
+//			} else {
+//				int count = 1;
+//				for (int col = 0; col < (double) numberOfColumns / 2; col++) {
+//					if (cabin[col][row] != 100) {
+//						if (alternating) {
+//							// int store=cabin[col][row];
+//							for (int col2 = col; col2 < numberOfColumns - count; col2++) {
+//								int store1 = cabin[col2][row];
+//								cabin[col2][row] = cabin[col2 + 1][row];
+//								cabin[col2 + 1][row] = store1;
+//								alternating = true;
+//							}
+//							// cabin[numberOfColumns-col-1][row]=store;
+//							count++;
+//						} else {
+//							alternating = true;
+//						}
+//					}
+//				}
+//			}
+//			// determine numberOfBagsLeft and Right
+//			int numberOfBagsLeft = 0;
+//			int numberOfBagsRight = 0;
+//			for (int col = 0; col < numberOfColumns; col++) {
+//				if (col < numberOfColumns / 2) {
+//					numberOfBagsLeft = numberOfBagsLeft + cabin[col][row];
+//				}
+//				if (numberOfColumns % 2 == 0 && col >= numberOfColumns / 2) {
+//					numberOfBagsRight = numberOfBagsRight + cabin[col][row];
+//				}
+//				if (col > numberOfColumns / 2 && numberOfColumns % 2 != 0) {
+//					numberOfBagsRight = numberOfBagsRight + cabin[col][row];
+//				}
+//			}
+//			// check even balance Left/Right
+//			if (numberOfBagsLeft > numberOfBagsRight ^ numberOfBagsRight > numberOfBagsLeft) {
+//				boolean alternate = false;
+//				if (numberOfBagsLeft > numberOfBagsRight + 1) {
+//					// switch two elements nearest to the middle != 0
+//					boolean firstTime = true;
+//					int store = 0;
+//					int storeColNumber = 0;
+//					for (int col = 0; col < numberOfColumns; col++) {
+//						if (cabin[col][row] == 0 && firstTime) {
+//							store = cabin[col - 1][row];
+//							storeColNumber = col - 1;
+//							firstTime = false;
+//						}
+//						if (!firstTime && cabin[col][row] != 0) {
+//							cabin[storeColNumber][row] = cabin[col - 1][row];
+//							cabin[col - 1][row] = store;
+//							numberOfBagsRight++;
+//						}
+//					}
+//				}
+//				if (numberOfBagsRight > numberOfBagsLeft + 1) {
+//					// switch two elements nearest to the middle != 0
+//					boolean firstTime = true;
+//					int store = 0;
+//					int storeColNumber = 0;
+//					for (int col = 0; col < numberOfColumns; col++) {
+//						if (cabin[col][row] == 0 && firstTime) {
+//							store = cabin[col - 1][row];
+//							storeColNumber = col - 1;
+//							firstTime = false;
+//						}
+//						if (!firstTime && cabin[col][row] != 0) {
+//							cabin[storeColNumber][row] = cabin[col - 1][row];
+//							cabin[col - 1][row] = store;
+//							numberOfBagsLeft++;
+//						}
+//					}
+//				} else if (alternate) {
+//					// switch two elements nearest to the middle != 0
+//					boolean firstTime = true;
+//					int store = 0;
+//					int storeColNumber = 0;
+//					for (int col = 0; col < numberOfColumns; col++) {
+//						if (cabin[col][row] == 0 && firstTime) {
+//							store = cabin[col - 1][row];
+//							storeColNumber = col - 1;
+//							firstTime = false;
+//						}
+//						if (!firstTime && cabin[col][row] != 0) {
+//							cabin[storeColNumber][row] = cabin[col - 1][row];
+//							cabin[col - 1][row] = store;
+//						}
+//					}
+//					alternate = false;
+//				} else
+//					alternate = true;
+//				// check middle seat in middle section empty
+//				if (numberOfColumns % 2 != 0) {
+//					if (cabin[numberOfColumns / 2][row] != 0
+//							&& (cabin[numberOfColumns / 2 + 1][row] == 0 || cabin[numberOfColumns / 2 - 1][row] == 0)) {
+//						if (cabin[numberOfColumns / 2 - 1][row] == 0) {
+//							// swap with middle seat
+//							if (cabin[numberOfColumns / 2 + 1][row] == 0) {
+//								if (numberOfBagsLeft > numberOfBagsRight) {
+//									cabin[numberOfColumns / 2 + 1][row] = cabin[numberOfColumns / 2][row];
+//									cabin[numberOfColumns / 2][row] = 0;
+//								} else {
+//									cabin[numberOfColumns / 2 - 1][row] = cabin[numberOfColumns / 2][row];
+//									cabin[numberOfColumns / 2][row] = 0;
+//								}
+//							} else {
+//								cabin[numberOfColumns / 2 - 1][row] = cabin[numberOfColumns / 2][row];
+//								cabin[numberOfColumns / 2][row] = 0;
+//							}
+//						} else {
+//							cabin[numberOfColumns / 2 + 1][row] = cabin[numberOfColumns / 2][row];
+//							cabin[numberOfColumns / 2][row] = 0;
+//						}
+//					}
+//				}
+//			}
+//		}
+//		// print luggage distribution
+//		System.out.println();
+//		for (int x = 0; x < numberOfRows; x++) {
+//			for (int y = 0; y < numberOfColumns; y++) {
+//				System.out.print(cabin[y][x] + " ");
+//			}
+//			System.out.println();
+//		}
+//
+//		// define seat list
+//		EList<Seat> seatList = new BasicEList<Seat>();
+//		for (int pax = 0; pax < paxList.size(); pax++) {
+//			seatList.add(paxList.get(pax).getSeat());
+//		}
+//
+//		// assign passengers to seats
+//		Seat seat = null;
+//		EList<Passenger> paxListHelp = new BasicEList<Passenger>();
+//		paxListHelp.addAll(paxList);
+//		EList<Passenger> paxListNew = new BasicEList<Passenger>();
+//		// loop through all rows
+//		for (int row = 0; row < numberOfRows; row++) {
+//			// loop through all columns
+//			for (int col = 0; col < numberOfColumns; col++) {
+//				// loop through pasenger list
+//				if (cabin[col][row] != 100) {
+//					// no passenger assigned to this spot
+//					boolean notAsigned = true;
+//					for (int paxID = 0; paxID < paxListHelp.size(); paxID++) {
+//						Passenger pax = paxListHelp.get(paxID);
+//						// search for a passenger who has the amount of luggage
+//						// placed at this seat
+//						if (cabin[col][row] == pax.getLuggage().size() && notAsigned) {
+//							// get seat object at current cabin position
+//							for (int seatID = 0; seatID < seatList.size(); seatID++) {
+//								if (seatList.get(seatID).getPassenger().getSeat().getId() == row + col && notAsigned) {
+//									seat = seatList.get(seatID);
+//									seat.setPassenger(pax);
+//									paxListHelp.remove(pax);
+//									paxListNew.add(pax);
+//									seatList.remove(seat);
+//									notAsigned = false;
+//								}
+//							}
+//						}
+//					}
+//				}
+//			}
+//		}
+//		// sort paxList
+//		for (int o = 0; o < paxListNew.size(); o++) {
+//			Passenger pax1 = paxListNew.get(o);
+//			for (int p = 0; p < paxList.size(); p++) {
+//				Passenger pax2 = paxList.get(p);
+//				if (pax1.getSeat().getId() == pax2.getSeat().getId()) {
+//					paxList.move(o, pax2);
+//				}
+//			}
+//		}
+//	}
 }
--- com.paxelerate.simulation/src/com/paxelerate/core/simulation/agent/action/StowLuggage.java
+++ com.paxelerate.simulation/src/com/paxelerate/core/simulation/agent/action/StowLuggage.java
@@ -52,4 +52,234 @@ public interface StowLuggage {
 		agent.setAlreadyStowed(true);
 
 	}
+
+//	/**
+//	 * Attempt to stow luggage. Simulates time for checking available space
+//	 */
+//	static void attemptToStowLuggage(Agent agent) {
+//		int sleepTime = 10;
+//		sleepTime += PassengerExtensions.getLuggageStowingTime(agent.getPassenger());
+//		StowLuggage.sleepAgentThread(sleepTime, agent);
+////		SimulationHandler.addPassengersLookingForLuggageSpot(agent.getPassenger());
+//		System.out.println("Stow Luggage: Looking for luggage spot deactivated!");
+//	}
+
+//	/**
+//	 * search for empty bin space in cabin
+//	 *
+//	 * @return
+//	 **/
+//	static LuggageStorage getAnyEmptyLuggageElement(Agent agent) {
+//
+//		for (LuggageStorage bin : DeckExtensions.getBy(agent.getPassenger()).getLuggageStorages()) {
+//
+//			int passengersLargeBags = StowLuggage.getNumberOfLargeBags(agent.getPassenger().getLuggage());
+//
+//			if (bin.getFreeVolume() > StowLuggage.getRequiredLuggageVolume(agent) && bin.getMaximumLargeBags()
+//					- StowLuggage.getNumberOfLargeBags(bin.getLuggage()) >= passengersLargeBags) {
+//				return bin;
+//			}
+//		}
+//		System.out.println("NOT IMPLEMENTED!! All bins are full, bags are added to residual list");
+//		return null;
+//	}
+
+//	/**
+//	 * Gets the bulkiness factor.
+//	 *
+//	 * @param type the type
+//	 * @return the bulkiness factor
+//	 */
+//	static double getBulkinessFactor(LuggageType type, LuggageProperties options) {
+//		switch (type) {
+//		case JACKET:
+//			return options.getJacketBulkiness();
+//		case SMALL_BAG:
+//			return options.getSmallBagBulkiness();
+//		case MEDIUM_BAG:
+//			return options.getMediumBagBulkiness();
+//		case LARGE_BAG:
+//			return options.getLargeBagBulkiness();
+//		default:
+//			return 1;
+//		}
+//	}
+
+//	/**
+//	 *
+//	 * @return
+//	 */
+//	static LuggageStorage getEmptyCLSElement(Agent agent) {
+//		LuggageStorage centralStoragesAtDoor = AgentFunctions
+//				.getCenterLuggageStorageAtDoor(agent.getPassenger().getDoor());
+//
+//		double requiredLuggageVolume = StowLuggage.getRequiredLuggageVolume(agent);
+//
+//		int largeBagsInBin = StowLuggage.getNumberOfLargeBags(centralStoragesAtDoor.getLuggage());
+//		int passengersLargeBags = StowLuggage.getNumberOfLargeBags(agent.getPassenger().getLuggage());
+//
+//		if (centralStoragesAtDoor.getFreeVolume() > requiredLuggageVolume
+//				&& centralStoragesAtDoor.getMaximumLargeBags() - largeBagsInBin >= passengersLargeBags) {
+//			return centralStoragesAtDoor;
+//		}
+//
+//		System.out.println("Center luggage storage at door " + agent.getPassenger().getDoor().getId()
+//				+ " are already full - Luggage is stowed in overhead bins at the seat");
+//		return null;
+//	}
+
+//	/**
+//	 * check for free bin, associated with row -> notify if full
+//	 *
+//	 * @return the empty overheadbin
+//	 */
+//	static LuggageStorage getEmptyLinkedLuggageElement(Agent agent) {
+//		EList<LuggageStorage> associatedStorages = EObjectHelper.getParent(Row.class, agent.getPassenger().getSeat())
+//				.getLuggageStorage();
+//		double requiredLuggageVolume = StowLuggage.getRequiredLuggageVolume(agent);
+//		double yAgentPosition = agent.getPassenger().getCurrentPosition().getY()
+//				* agent.getHandler().getSettings().getSimulationGridResolution();
+//
+//		if (!associatedStorages.isEmpty()) {
+//			for (LuggageStorage bin : associatedStorages) {
+//
+//				// Bin should be within 1.2m reach in y direction, required for twin aisle
+//				// configurations
+//				if (Math.abs(yAgentPosition - bin.getPosition().getY()) < 120) {
+//
+//					int passengersLargeBags = StowLuggage.getNumberOfLargeBags(agent.getPassenger().getLuggage());
+//
+//					if (bin.getFreeVolume() > requiredLuggageVolume && bin.getMaximumLargeBags()
+//							- StowLuggage.getNumberOfLargeBags(bin.getLuggage()) >= passengersLargeBags) {
+//						return bin;
+//					}
+//				}
+//			}
+//			System.out.println("Linked overhead bins in row "
+//					+ RowExtensions.getRowNumber(EObjectHelper.getParent(Row.class, agent.getPassenger().getSeat()))
+//					+ " are already full - Luggage is stowed somewhere else");
+//			return null;
+//			// return getAnyEmptyLuggageElement(agent);
+//		}
+//		System.out.println("No luggage storage found in row: "
+//				+ RowExtensions.getRowNumber(EObjectHelper.getParent(Row.class, agent.getPassenger().getSeat())));
+//		return null;
+//	}
+
+//	/**
+//	 * Gets the number of large bags.
+//	 *
+//	 * @param items the luggage items
+//	 * @return the number of large bags
+//	 */
+//	static int getNumberOfLargeBags(EList<Luggage> items) {
+//		return (int) items.stream().filter(l -> l.getType() == LuggageType.LARGE_BAG).count();
+//	}
+
+//	/**
+//	 * Gets the required luggage volume.
+//	 *
+//	 * @return the required luggage volume
+//	 */
+//	static double getRequiredLuggageVolume(Agent agent) {
+//		return agent
+//				.getPassenger().getLuggage().stream().mapToDouble(l -> l.getVolume() * StowLuggage
+//						.getBulkinessFactor(l.getType(), agent.getHandler().getSettings().getLuggageProperties()))
+//				.sum();
+//	}
+//
+//	/**
+//	 *
+//	 * @param agent
+//	 * @param centerLuggageStorage
+//	 */
+//	static void run(Agent agent, boolean centerLuggageStorage) {
+//
+//		// Skip luggage stowing and just add a time penalty
+//		if (agent.getHandler().getSettings().getLuggageModelOption() == LuggageModelOption.SIMPLE) {
+//		StowLuggage.sleepAgentThread(PassengerExtensions.getLuggageStowingTime(agent.getPassenger()), agent);
+//
+//		agent.setAlreadyStowed(true);
+//
+//		} else {
+//			if (centerLuggageStorage) {
+//				LuggageStorage bin = StowLuggage.getEmptyCLSElement(agent);
+//				if (bin != null) {
+//					StowLuggage.sleepAgentThread(StowLuggage.simulateStowingProcess(bin, agent), agent);
+//					StowLuggage.stowBagsInBin(bin, agent);
+//					agent.setAlreadyStowed(true);
+//				}
+//			} else {
+//				LuggageStorage bin = StowLuggage.getEmptyLinkedLuggageElement(agent);
+//				if (bin != null) { // && !linkedBinsFull) {
+//					StowLuggage.sleepAgentThread(StowLuggage.simulateStowingProcess(bin, agent), agent);
+//					StowLuggage.stowBagsInBin(bin, agent);
+//				} else {
+//					StowLuggage.attemptToStowLuggage(agent);
+//				}
+//				/* notify everyone that the luggage is now stowed */
+//				agent.setAlreadyStowed(true);
+//			}
+//		}
+//}
+
+//	/**
+//	 * Simulate stowing process.
+//	 *
+//	 * @param bin
+//	 */
+//	static double simulateStowingProcess(LuggageStorage bin, Agent agent) {
+//
+//		double stowingTime = PassengerExtensions.getLuggageStowingTime(agent.getPassenger());
+//
+//		// stowing time is adapted according to the bin filling degree: > 60% - 1.2; >=
+//		// 80% - 1.5
+//		int binFillingDegree = (int) (100 - bin.getFreeVolume() * 100 / bin.getNetVolume());
+//		if (binFillingDegree > 60 && binFillingDegree < 80) {
+//			stowingTime = stowingTime * 1.2;
+//		} else if (binFillingDegree >= 80) {
+//			stowingTime = stowingTime * 1.5;
+//		}
+//		return stowingTime;
+//	}
+//
+//	/**
+//	 * Sleep agent thread.
+//	 *
+//	 * @param time the time
+//	 */
+//	static void sleepAgentThread(double sleepTime, Agent agent) {
+//
+//		// change state
+//		agent.getPassenger().setState(State.STOW_LUGGAGE);
+//
+//		// change influence area
+//		agent.blockArea(agent.getPassenger().getCurrentPosition(), false, false);
+//		agent.blockArea(agent.getPassenger().getCurrentPosition(), true, false);
+//
+//		// agent.rotateTo(180);
+//
+//		try {
+//			Thread.sleep(agent.time(sleepTime));
+//			agent.getPassenger().setTotalTimeWaited(agent.getPassenger().getTotalTimeWaited() + agent.time(sleepTime));
+//		} catch (InterruptedException e) {
+//			System.out.println("Agent action: InterruptedException");
+//		}
+//	}
+//
+//	/**
+//	 * Links the bags to the OverheadBin and calculates the remaining volume; if not
+//	 * enough space is available
+//	 *
+//	 * @param bin the OverheadBin
+//	 */
+//	static void stowBagsInBin(LuggageStorage bin, Agent agent) {
+//
+//		for (Luggage bag : agent.getPassenger().getLuggage()) {
+//			bin.getLuggage().add(bag);
+//		}
+//
+//		bin.setFreeVolume(bin.getFreeVolume() - StowLuggage.getRequiredLuggageVolume(agent));
+//	}
+//}
 }
